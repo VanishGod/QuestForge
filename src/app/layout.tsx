0cdd5b3deff5cbdcd7e35f877f75250c4a344dc8
VanishGod <lianperezchacon@gmail.com>
2025-08-30 00:36:36 -0400

    tracking src folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "./home.modules.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <h1>Hay durofrio?</h1>
         {children}
       </body>
     </html>
